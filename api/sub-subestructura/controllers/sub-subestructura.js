a6a9a8a0bc0bb275f5f81b940cecab96813fa130
Sergio Alvarez Diaz <cur3n4@gmail.com>
2022-11-10 00:46:42 +0100

    First draft
    - Public UI

--- api/sub-subestructura/controllers/sub-subestructura.js
+++ api/sub-subestructura/controllers/sub-subestructura.js
@@ -1,8 +1,41 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+/*
+  /!**
+   * Retrieve records.
+   *
+   * @return {Array}
+   *!/
+
+  async find(ctx) {
+    console.error("find")
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.sub_subestructura.search(ctx.query);
+    } else {
+      entities = await strapi.services.sub_subestructura.find(ctx.query);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.sub_subestructura }));
+  },
+
+  /!**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   *!/
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services['sub-subestructura'].findOne({ id });
+    return sanitizeEntity(entity, { model: strapi.models.sub_subestructura, includeFields:   });
+  }*/
+};
