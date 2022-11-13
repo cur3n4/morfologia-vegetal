cfcfe665fb065e131b1b7a08c583197c1b7b820f
Sergio Alvarez Diaz <cur3n4@gmail.com>
2022-11-13 10:37:45 +0100

    First draft
    - Tidy up

---
+++ public/assets/js/alpine-component.js
@@ -0,0 +1,43 @@
+document.addEventListener('alpine:init', () => {
+  class ComponentWrapper extends HTMLElement {
+    connectedCallback() {
+      let shadow = this.attachShadow({ mode: 'open' })
+
+      if (this.attributes.template) {
+        let template = document.getElementById(this.attributes.template.value)
+        console.log('has tempalte')
+        let component = new DOMParser().parseFromString(
+          template.innerHTML,
+          'text/html'
+        ).body.children[0];
+        shadow.appendChild(component)
+
+        document.addEventListener('alpine:initialized', () =>
+          Alpine.initTree(shadow)
+        )
+      }
+
+      if (this.attributes.url) {
+        fetch(this.attributes.url.value)
+          .then((response) => response.text())
+          .then((template) => {
+            let component = new DOMParser().parseFromString(
+              template,
+              'text/html'
+            ).body.children[0];
+            shadow.appendChild(component)
+
+            Alpine.initTree(shadow)
+          })
+      }
+    }
+  }
+
+  if (window.customElements.get('x-component-wrapper')) return
+
+  customElements.define('x-component-wrapper', ComponentWrapper)
+
+  Alpine.directive('component', () => {
+    new ComponentWrapper()
+  })
+});
